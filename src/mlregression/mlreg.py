ded3b4a2234e542ec902cf09d2668ad27e1cd457
Nicolaj Søndergaard Mühlbach <n.muhlbach@gmail.com>
2021-11-16 08:47:33 -0500

    Updated mlreg

--- src/mlregression/mlreg.py
+++ src/mlregression/mlreg.py
@@ -3,57 +3,44 @@
 #------------------------------------------------------------------------------
 # Standard
 import numpy as np
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import ElasticNet
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, KFold, TimeSeriesSplit
 
-class A():
-    pass
-
-self = A()
-
-self.estimator = RandomForestRegressor()
+# User
+from .base.base_mlreg import BaseMLRegressor
 
 #------------------------------------------------------------------------------
 # MLRegressor
 #------------------------------------------------------------------------------
-# class MLRegressor():
-#     """
-#     This class implements the mlreg command
-#     """
-#     # --------------------
-#     # Constructor function
-#     # --------------------
-#     # HERE!
-#     def __init__(self,
-#                  estimator=ConstrainedOLS(),
-#                  param_grid={'coefs_lower_bound':0,
-#                              'coefs_lower_bound_constraint':">=",
-#                              'coefs_sum_bound':1,
-#                              'coefs_sum_bound_constraint':"<=",},
-#                  cv_params={'scoring':None,
-#                             'n_jobs':None,
-#                             'refit':True,
-#                             'verbose':0,
-#                             'pre_dispatch':'2*n_jobs',
-#                             'random_state':None,
-#                             'error_score':np.nan,
-#                             'return_train_score':False},
-#                  n_folds=3,
-#                  fold_type="KFold",
-#                  max_n_models=50,
-#                  test_size=0.25,
-#                  verbose=False,
-#                  ):
-#         super().__init__(
-#             estimator=estimator,
-#             param_grid=param_grid,
-#             cv_params=cv_params,
-#             n_folds=n_folds,
-#             fold_type=fold_type,
-#             max_n_models=max_n_models,
-#             test_size=test_size,
-#             verbose=verbose,
-#             )
+class MLRegressor(BaseMLRegressor):
+    """
+    This class implements the mlreg command
+    """
+    # --------------------
+    # Constructor function
+    # --------------------
+    def __init__(self,
+               estimator,
+               param_grid=None,
+               cv_params={'scoring':None,
+                          'n_jobs':None,
+                          'refit':True,
+                          'verbose':0,
+                          'pre_dispatch':'2*n_jobs',
+                          'random_state':None,
+                          'error_score':np.nan,
+                          'return_train_score':False},
+               n_folds=3,
+               test_size=None,
+               fold_type="KFold",
+               max_n_models=50,
+               verbose=False,
+                  ):
+        super().__init__(
+            estimator=estimator,
+            param_grid=param_grid,
+            cv_params=cv_params,
+            n_folds=n_folds,
+            test_size=test_size,
+            fold_type=fold_type,
+            max_n_models=max_n_models,
+            verbose=verbose)
 
